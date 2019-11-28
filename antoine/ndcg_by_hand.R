bea8a296c7c1725d2e5cc49aed254f53b3deedb9
antoine_tixier <anti5662@lix.polytechnique.fr>
2019-11-28 09:51:13 +0100

    update

--- antoine/ndcg_by_hand.R
+++ antoine/ndcg_by_hand.R
@@ -3,6 +3,8 @@ dcg = function(x) {
   sum(unlist(lapply(1:length(x), function(i) (2^x[i] - 1)/log(i+1))))
 }
 
+# = = = = = 
+
 ours = c(7,9,8,10,6,5,4,3,2,1)
 truth = rev(1:10)
 
@@ -13,11 +15,21 @@ dcg(ours)/dcg(truth)
 truth = rev(1:100)
 dcg_truth = dcg(truth)
 
-scores = list()
-for (run in 1:200){
+scores = lapply(1:200, function(run) {
   ours = sample(1:100,100)
-  scores[[run]] = dcg(ours)/dcg_truth
-}
+  dcg(ours)/dcg_truth
+})
+
+mean(unlist(scores))
+
+
+truth = rev(1:100) + 900 # the values of the scores do not change the results (+-900)
+dcg_truth = dcg(truth)
+
+scores = lapply(1:200, function(run) {
+  ours = sample(1:100,100) + 900
+  dcg(ours)/dcg_truth
+})
 
 mean(unlist(scores))
 

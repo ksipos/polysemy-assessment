9b889d738714b8eb91a25da5f1de6f114d539c7b
ksipos <ksipos@windowslive.com>
2020-03-18 13:05:31 +0100

    Updated dim_reduction

--- dim_reduction/README.md
+++ dim_reduction/README.md
@@ -1,4 +1,17 @@
 # Dimensionality Reduction
 
-Given the vectors generated from ElMo (#words x 1024) we implemented a pyspark based dimensionality reduction approach.
+Due to the high dimensionality of our input we had to use a PySpark based implementation to reduce it.
 
+## Input
+
+Our input is a list containing the generated ELMo vectors (1024 dimensions).
+Each vectors has to be formated following this example:
+
+*word - 0.6579854,1.161026,0.43898278,[...],2.0629232,0.063231304*
+
+
+## Output
+
+The output is formated as following:
+
+*write,"-8.898457162048345,-0.08643378936279938"*
--- dim_reduction/pca.py
+++ dim_reduction/pca.py
@@ -1,27 +1,35 @@
-# pyspark --master yarn --executor-cores 2 --driver-cores 10 --num-executors 15 --executor-memory 10G --driver-memory 30G
-
-from pyspark.ml.linalg import VectorUDT
-from pyspark.sql.functions import *
-from pyspark.ml.linalg import DenseVector
+# PySpark parameters used
+# --executor-cores 2 --driver-cores 10 -num-executors 15 --executor-memory 10G --driver-memory 30G
+from pyspark.ml.linalg import DenseVector, VectorUDT
 from pyspark.ml.feature import PCA
+from pyspark.sql.functions import *
 
-vectorsF = sc.textFile("sparkvectors2019") 
-vectorsR = vectorsF.map(lambda row: (row.split(" - ")[0], row.split(" - ")[1]))
+# Each line of the Input file follows this format:
+# "word - 0.6579854,1.161026,0.43898278,[...],2.0629232,0.063231304"
+input_file = "sparkvectors2019"
 
-vectorsDF = spark.createDataFrame(vectorsR, ['Label', 'Vector'])
+# Maximum PCA dimension
+K = 20
 
-vectorsDF.count()
+# Read the file, parse it as RDD and transform it into DataFrame
+raw_vectors = sc.textFile(input_file)
+vectors_rdd = raw_vectors.map(lambda row:
+                              (row.split(" - ")[0], row.split(" - ")[1]))
+vectors_df = spark.createDataFrame(vectors_rdd, ['Label', 'Vector'])
 
-one = udf(lambda row: DenseVector([float(number) for number in row.split(",")]), VectorUDT())
-vecPCA = vectorsDF.withColumn("DenseVector", one(vectorsDF['Vector']))
+# Count the elements to force Spark to load the file at this time
+vectors_df.count()
 
+# Parse vectors as DenseVectors
+vectorize_lines = udf(lambda row:
+                      DenseVector([float(number) for number in row.split(",")]), VectorUDT())
+vectorized_df = vectors_df.withColumn(
+    "DenseVector", vectorize_lines(vectors_df['Vector']))
 
-for k in range(2, 20, 2):
-    pca = PCA(k=k, inputCol="DenseVector", outputCol="pcaFeatures")
-    model = pca.fit(vecPCA)
-    model.explainedVariance
-    result = model.transform(vecPCA)
-    result.rdd.map(lambda row: row[0] + ",\"" + ",".join(str(a) for a in row[3]) + "\"").saveAsTextFile("version2_pca" + str(k))
-
-
-
+pca = PCA(k=K, inputCol="DenseVector", outputCol='pcaFeatures')
+model = pca.fit(vectorized_df)
+# model.explainedVariance
+result = mode.transform(vectorized_df)
+for k, in range(1, K):
+    result.rdd.map(lambda row:
+                   row[0] + ",\"" + ",".join(str(a) for a in row[3][:k]) + "\"").saveAsTextFile("pca" + str(k))
